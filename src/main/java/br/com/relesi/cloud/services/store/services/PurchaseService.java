9e3a343e72b40bc0d8f1aed9336101d48383d067
relesi <renatolessa.2011@hotmail.com>
2021-04-09 18:56:08 -0400

    implement service

--- src/main/java/br/com/relesi/cloud/services/store/services/PurchaseService.java
+++ src/main/java/br/com/relesi/cloud/services/store/services/PurchaseService.java
@@ -2,11 +2,13 @@ package br.com.relesi.cloud.services.store.services;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.relesi.cloud.services.store.dto.InfoProviderDTO;
 import br.com.relesi.cloud.services.store.dto.PurchaseDTO;
 
+@Service
 public class PurchaseService {
 
 	public void accomplishPurchase(PurchaseDTO purchase) {
@@ -16,7 +18,7 @@ public class PurchaseService {
 				client.exchange("http://localhost:8081/info/" + purchase.getAddress().getState(), 
 				HttpMethod.GET, null,InfoProviderDTO.class);
 		
-		//System.out.println(exchange.getBody().getAddress());
+		System.out.println(exchange.getBody().getAddress());
 	}
 
 }
