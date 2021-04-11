a56b229906d162ef292dd18c7d980db0344bcb95
relesi <renatolessa.2011@hotmail.com>
2021-04-11 16:02:23 -0300

    implement loadbalance to request service

--- src/main/java/br/com/relesi/cloud/services/store/services/PurchaseService.java
+++ src/main/java/br/com/relesi/cloud/services/store/services/PurchaseService.java
@@ -1,5 +1,6 @@
 package br.com.relesi.cloud.services.store.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,13 @@ import br.com.relesi.cloud.services.store.dto.PurchaseDTO;
 
 @Service
 public class PurchaseService {
+	
+	@Autowired
+	private RestTemplate client;
 
 	public void accomplishPurchase(PurchaseDTO purchase) {
 
-		RestTemplate client = new RestTemplate();
+		
 		ResponseEntity<InfoProviderDTO> exchange =	
 				client.exchange("http://provider/info/" + purchase.getAddress().getState(), 
 				HttpMethod.GET, null,InfoProviderDTO.class);
