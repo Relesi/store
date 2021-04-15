42d2569630241a9fc14c33faf88e7989f5f591b5
relesi <renatolessa.2011@hotmail.com>
2021-04-15 18:52:17 -0300

    implement Client Side Load Balancer test

--- src/main/java/br/com/relesi/cloud/services/store/services/PurchaseService.java
+++ src/main/java/br/com/relesi/cloud/services/store/services/PurchaseService.java
@@ -1,10 +1,13 @@
 package br.com.relesi.cloud.services.store.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+
 
 import br.com.relesi.cloud.services.store.dto.InfoProviderDTO;
 import br.com.relesi.cloud.services.store.dto.PurchaseDTO;
@@ -14,6 +17,9 @@ public class PurchaseService {
 	
 	@Autowired
 	private RestTemplate client;
+	
+	@Autowired
+	private DiscoveryClient eurekaClient;
 
 	public void accomplishPurchase(PurchaseDTO purchase) {
 
@@ -21,6 +27,11 @@ public class PurchaseService {
 		ResponseEntity<InfoProviderDTO> exchange =	
 				client.exchange("http://provider/info/" + purchase.getAddress().getState(), 
 				HttpMethod.GET, null,InfoProviderDTO.class);
+		
+		eurekaClient.getInstances("provider").stream()
+		.forEach(provider -> {
+			System.out.println("localhost:" + provider.getPort());
+		});
 		
 		System.out.println(exchange.getBody().getAddress());
 	}
