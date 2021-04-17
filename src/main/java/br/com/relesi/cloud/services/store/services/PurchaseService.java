4359fd4d59106ddd98b5396902091c615a2e57d8
relesi <renatolessa.2011@hotmail.com>
2021-04-17 20:05:04 -0300

    Implement Distributed Tracing

--- src/main/java/br/com/relesi/cloud/services/store/services/PurchaseService.java
+++ src/main/java/br/com/relesi/cloud/services/store/services/PurchaseService.java
@@ -2,6 +2,8 @@ package br.com.relesi.cloud.services.store.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import br.com.relesi.cloud.services.store.client.ProviderClient;
 import br.com.relesi.cloud.services.store.dto.InfoOrderDto;
@@ -11,14 +13,20 @@ import br.com.relesi.cloud.services.store.model.Purchase;
 
 @Service
 public class PurchaseService {
-
+	
+	private static final Logger LOG = LoggerFactory.getLogger(PurchaseService.class);
+	
 	@Autowired
 	private ProviderClient providerClient;
 
 	public Purchase accomplishPurchase(PurchaseDTO purchase) {
 
+		final String state = purchase.getAddress().getState();
+		
+		LOG.info("Seeking provider information by {}", state);
 		InfoProviderDTO info = providerClient.getInfoToState(purchase.getAddress().getState());
 
+		LOG.info("Placing order");
 		InfoOrderDto infoOrder = providerClient.placeOrder(purchase.getItems());
 
 		Purchase purchaseSave = new Purchase();
