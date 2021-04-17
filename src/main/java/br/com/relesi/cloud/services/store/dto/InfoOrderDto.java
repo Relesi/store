a0df9dbc8ed6f99e0aad5cd23048aafb74576826
relesi <renatolessa.2011@hotmail.com>
2021-04-17 17:53:45 -0300

    implement integration store and provider

---
+++ src/main/java/br/com/relesi/cloud/services/store/dto/InfoOrderDto.java
@@ -0,0 +1,24 @@
+package br.com.relesi.cloud.services.store.dto;
+
+public class InfoOrderDto {
+
+	private Long id;
+	private Integer preparation;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getPreparation() {
+		return preparation;
+	}
+
+	public void setPreparation(Integer preparation) {
+		this.preparation = preparation;
+	}
+
+}
--- src/main/java/br/com/relesi/cloud/services/store/services/PurchaseService.java
+++ src/main/java/br/com/relesi/cloud/services/store/services/PurchaseService.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.relesi.cloud.services.store.client.ProviderClient;
+import br.com.relesi.cloud.services.store.dto.InfoOrderDto;
 import br.com.relesi.cloud.services.store.dto.InfoProviderDTO;
 import br.com.relesi.cloud.services.store.dto.PurchaseDTO;
+import br.com.relesi.cloud.services.store.model.Purchase;
 
 @Service
 public class PurchaseService {
@@ -13,12 +15,20 @@ public class PurchaseService {
 	@Autowired
 	private ProviderClient providerClient;
 
-	public void accomplishPurchase(PurchaseDTO purchase) {
+	public Purchase accomplishPurchase(PurchaseDTO purchase) {
 
 		InfoProviderDTO info = providerClient.getInfoToState(purchase.getAddress().getState());
-		
-		
+
+		InfoOrderDto infoOrder = providerClient.placeOrder(purchase.getItems());
+
+		Purchase purchaseSave = new Purchase();
+		purchaseSave.setOrderDemand(infoOrder.getId());
+		purchaseSave.setPreparation(infoOrder.getPreparation());
+		purchaseSave.setDestinationAddress(info.getAddress().toString());
+
 		System.out.println(info.getAddress());
+
+		return purchaseSave;
 	}
 
 }
