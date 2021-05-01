12dafc7d47bc7e1cc28ba876791a73dfa5648bfa
relesi <renatolessa.2011@hotmail.com>
2021-05-01 06:00:07 -0300

    implement treatment error to integration

--- src/main/java/br/com/relesi/cloud/services/store/model/Purchase.java
+++ src/main/java/br/com/relesi/cloud/services/store/model/Purchase.java
@@ -1,26 +1,33 @@
 package br.com.relesi.cloud.services.store.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Purchase {
+public class Purchase implements Serializable {
+
+	private static final long serialVersionUID = 8841683791965183708L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Id
 	private Long orderDemand;
 
 	private Integer preparation;
 	private String destinationAddress;
 	private LocalDate dateForDelivery;
 	private Long voucher;
+
+	@Enumerated(EnumType.STRING)
+	private PurchaseState state;
 
 	public Long getId() {
 		return id;
@@ -68,6 +75,14 @@ public class Purchase {
 
 	public void setVoucher(Long voucher) {
 		this.voucher = voucher;
+	}
+
+	public PurchaseState getState() {
+		return state;
+	}
+
+	public void setState(PurchaseState state) {
+		this.state = state;
 	}
 
 }
